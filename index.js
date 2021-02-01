e7bf560f3ddb042bc2758e5da12f8e6a0cb8586c
Tristan Nelson <Tristan@tristans-mbp.lan>
2021-01-31 21:48:41 -0500

    I modified my cheat detector so that it is a bit smarter and will explain the unique contradiction to the user. I also added > symbol to all instances that require user input to make it a bit clearer. This will likely be my final submission and perhaps will further refactor later on

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ function ask(questionText) {
     rl.question(questionText, resolve);
   });
 }
-
 //creates an empty array and fills it with a range determined by the two parameters
 function guessRange(startRange, endRange) {
   let arrayRange = [];
@@ -44,6 +43,13 @@ function median(array) {
     : array[Math.floor(array.length / 2)];
 }
 
+//accepts two arguments and  produces a random integer
+function randomInteger(min, max) {
+  let range = max - min + 1;
+  let randInt = Math.floor(Math.random() * range);
+  return randInt;
+}
+
 //async function that runs a guessing numbers game
 async function start() {
   console.log(
@@ -51,10 +57,10 @@ async function start() {
   );
   //collects user input for the max range number and the users secret number that program must guess
   let max = await ask(
-    "Pick any number greater than 1 that will represent the high range.\nFor example if you pick 100, our game would be a number between 1-100.\n\nGot it? Ok, so what will the high range be: "
+    "Pick any number greater than 1 that will represent the high range.\nFor example if you pick 100, our game would be a number between 1-100.\n\nGot it? Ok, so what will the high range be:\n> "
   );
   let secretNumber = await ask(
-    "What is your secret number?\nI won't peek, I promise...\n"
+    "What is your secret number?\nI won't peek, I promise...\n>"
   );
   console.log("You entered: " + secretNumber);
   //setting up variables for the loop
@@ -65,25 +71,22 @@ async function start() {
   while (computerGuess !== secretNumber) {
     //loop that will continue iterating until program picks secret number
     counter++;
-    console.log(`min = ${min}`);
-    console.log(`max = ${max}`);
-    console.log(`Computer Guess = ${computerGuess}`);
-
     let range = guessRange(min, max);
     computerGuess = Math.floor(median(range));
+    //insert loop
+    let humanAnswer = await ask(
+      "Is this your number? " + computerGuess + "\nType No or Yes:\n> "
+    );
 
-    let humanAnswer = await ask("Is this your number? " + computerGuess + "\n");
     //conditional tree that evaluates input based on 4 options: Yes-No and High-Low
     //a break point for loop and conditional is if the input Yes or if the computer guess and secret number are the same but the user does not answer
-    console.log(`Computer Guess = ${computerGuess}`);
-
     if (humanAnswer === "No" && computerGuess == secretNumber) {
       //using equals and not identity because input is a string number and not an actual one (will fix type conversion is preferred over coercion)
       console.log(`You answered incorrectly. I chose correctly!`);
       break;
     } else if (humanAnswer === "Yes" && computerGuess != secretNumber) {
-      console.log(`You are not being truthy with me`);
-      process.exit();
+      console.log(`You are not being truthy with me! Let's run it back.`);
+      start();
     } else if (humanAnswer === "Yes") {
       break;
     }
@@ -92,20 +95,31 @@ async function start() {
 
     //cheat detector
     if (computerGuess === min + 1 && highOrLow === "L") {
-      console.log(`Hey no cheating!`);
-      process.exit();
+      console.log(
+        `Hey you contradicted yourself!\nYou said it was higher than ${
+          computerGuess - 1
+        } so it can't also be lower than ${computerGuess}.\nLet's start over\n`
+      );
+      start();
     } else if (computerGuess === max - 1 && highOrLow === "H") {
-      console.log(`Hey that can't be right! I don't play with cheaters`);
-      process.exit();
+      console.log(
+        `Hey you contradicted yourself!\nYou said it was lower than ${
+          computerGuess + 1
+        } so it can't also be higher than ${computerGuess}.\nLets start over\n`
+      );
+      start();
     } else if (
       (highOrLow === "L" && computerGuess < secretNumber) ||
       (highOrLow === "H" && computerGuess > secretNumber)
     ) {
-      console.log(`Hey that can't be right! Answer honestly.`);
+      console.log(
+        `Hey that can't be right! Answer honestly. We're starting over.\n`
+      );
+      start();
     }
 
+    //conditional processing Yes or No and High or Low input and updating the min and max accordingly
     if (humanAnswer === "No" && highOrLow === "H") {
-      //conditional processing Yes or No and High or Low input and updating the min and max accordingly
       min = computerGuess;
     } else if (humanAnswer === "No" && highOrLow === "L") {
       max = computerGuess;
@@ -116,18 +130,89 @@ async function start() {
   console.log("Your number was " + secretNumber);
   console.log("I guessed it in " + counter + " attempts");
   console.log("I won! Better luck next time");
-  let reload = await ask(`Do you want to play again? Type 'yes' or 'no' `);
-  if (reload === "no") {
+  let reload = await ask(`Do you want to play again? Type 'Yes' or 'No'\n>`);
+  if (reload === "No") {
     console.log(`Bummer`);
     process.exit();
   } else {
     return start();
   }
 }
-start();
+
+//async function that runs a guessing numbers game
+async function reverseGame() {
+  console.log(
+    "\nLet's play a game where I (computer) pick a number and you (human) try to guess it.\n"
+  );
+  //collects user input for the max range number and the users secret number that program must guess
+  console.log(
+    `I will pick a number between 1-100 and then you have to guess it.`
+  );
+
+  let secretNumber = randomInteger(1, 100);
+
+  //setting up variables for the loop
+  let min = 1;
+  let max = 100;
+  let counter = 0;
+  let humanGuess;
+
+  while (humanGuess !== secretNumber) {
+    //loop that will continue iterating until program picks secret number
+    counter++;
+
+    humanGuess = await ask("OK, guess a number:\n>");
+
+    //conditional tree that evaluates input based on 4 options: Yes-No and High-Low
+    //a break point for loop and conditional is if the input Yes or if the computer guess and secret number are the same but the user does not answer
+    if (humanGuess == secretNumber) {
+      //using equals and not identity because input is a string number and not an actual one (will fix, type conversion is best practice over coercion)
+      break;
+    } else if (humanGuess < min || humanGuess > max) {
+      console.log(
+        `\nYour number is not within the scope of this game.\nLet's start over.`
+      );
+      reverseGame();
+    }
+
+    //conditional processing Yes or No and High or Low input and updating the min and max accordingly
+    if (humanGuess < secretNumber) {
+      console.log(`Wrong guess. The number is higher than that`);
+    } else if (humanGuess > secretNumber) {
+      console.log(`Wrong guess. The number is lower than that`);
+    }
+  }
+
+  //after loop breaks there are three log statements that reveal the secret number, track how many guesses, and issue a conciliatory statement
+  console.log("My pick was " + secretNumber);
+  console.log("You guessed it in " + counter + " attempts");
+  console.log("You won!");
+  let reload = await ask(`Do you want to play again? Type 'Yes' or 'No'\n>`);
+  if (reload === "no") {
+    console.log(`Bummer`);
+    process.exit();
+  } else {
+    return reverseGame();
+  }
+}
 //to-do
 //sanitize user input
-//need to solve why I must use comparison and not identity with compGuess == secretNum or doesn't work
-//build multiple loops for high and Low and No or Yes
-//build new reversal game
-//combine reversal and normal - allow user to choose
+//need to convert input number to data type number - comes in as a string
+
+//game begins with a user choice to play the reverse or normal game
+async function chooseGame() {
+  console.log(
+    `We have two fun games for you to choose from.
+  Game #1 is our regular Guess The Number - User picks a random number and the Computer guesses
+  Game #2 is our reverse Guess The Number - Computer picks a random number and the User guesses`
+  );
+  //based on user input either the normal guess the game will be called or the reverse will be
+  let userPick = await ask(`Type: 1 for Game #1 or Type: 2 for Game #2\n> `);
+  if (userPick == 1) {
+    start();
+  } else {
+    reverseGame();
+  }
+}
+
+chooseGame();
